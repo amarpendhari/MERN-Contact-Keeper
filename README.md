33a473722f89f2f17600f5d3ed30204e5dc0a0df
Amar Pendhari <amar.pendhari97@gmail.com>
2020-07-27 22:26:13 +0530

    MERN Stack

---
+++ README.md
@@ -0,0 +1,37 @@
+# MERN Stack Contact Keeper
+
+![MERN Logo](/client/src/assets/MERN.png)
+
+## Project Specifications
+
+- Registration & Login Required to Access Contact Keeper App
+- JWT used for Authentication
+- Express Validator for Backend Validation
+- Handling CRUD operations on Client & Server side
+- Context API for React State Management
+- Multiple Context Implementation
+- Setting Proxy
+
+* Register
+
+  - Register User Page to Register a New User
+
+* Login
+
+- Login User Page to Login User
+- Registration Required to Login
+
+* Users
+
+  - Register User
+  - Login User with Email & Password
+  - Display Contact when User get Logged In
+  - Once user has Logged In, User can Add, Update & Delete Contacts
+  - User Contacts should be Private and not visible to other users
+  - User Should be able to Log Out.
+
+* Contact Form
+
+- All Feilds are required + Validation
+- Once Contact Added, Contact List will update
+- Added Contacts can be Edited & Deleted
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,160 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  GET_CONTACT,
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACT,
+  CLEAR_FILTER,
+  CONTACT_ERROR,
+  CLEAR_CONTACT
+} from "../types";
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [],
+    current: null,
+    filtered: null,
+    error: null
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Get Contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get("/api/contacts");
+      dispatch({
+        type: GET_CONTACT,
+        payload: res.data
+      });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg
+      });
+    }
+  };
+
+  // Add Contact
+  const addContact = async contact => {
+    const config = {
+      header: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.post("/api/contacts", contact, config);
+      dispatch({
+        type: ADD_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+  // Delete contact
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      header: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: contact
+      });
+      console.log(res.data);
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+
+  //Clear Contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACT });
+  };
+
+  // Set current Contact
+  const setCurrent = contact => {
+    dispatch({
+      type: SET_CURRENT,
+      payload: contact
+    });
+  };
+
+  // Clear current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+
+  // Filter Contact
+  const filterContacts = text => {
+    dispatch({
+      type: FILTER_CONTACT,
+      payload: text
+    });
+  };
+
+  // Clear filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
+        error: state.error,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
+        getContacts,
+        clearContacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
